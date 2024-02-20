fbca17b906e5b12eaae39d751223f8ae1d38e178
deep473 <deepztanshu@gmail.com>
2024-02-20 18:06:57 +0530

    tune hub june batch

--- src/main/java/com/example/demo/controller/UsersController.java
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -1,27 +1,76 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.demo.entities.LoginData;
+import com.example.demo.entities.Song;
 import com.example.demo.entities.Users;
+import com.example.demo.services.SongService;
 import com.example.demo.services.UsersService;
 
 import jakarta.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 
-
-@Controller
+@CrossOrigin("*")
+@RestController
 public class UsersController {
 	@Autowired
 	UsersService service;
 	
+	@Autowired
+	SongService songService;
+	
+	@PostMapping("/validate")
+	public String validate(@RequestBody LoginData data,
+			HttpSession session, Model model) {
+		
+		System.out.println("call received");
+		
+		String email = data.getEmail();
+		String password = data.getPassword();
+		
+		if(service.validateUser(email,password) == true) {
+			String role = service.getRole(email);
+			
+			session.setAttribute("email", email);
+			
+			if(role.equals("admin")) {
+				
+				return "adminHome";
+			} 
+			else {
+				Users user = service.getUser(email);
+				boolean userStatus = user.isPremium();
+				List<Song> songsList = songService.fetchAllSongs();
+				model.addAttribute("songs", songsList);
+				
+				model.addAttribute("isPremium", userStatus);
+				
+				return "customerHome";
+			}
+		}
+		else {
+			return "login";
+		}
+	}
+	
+	
+	
 	@PostMapping("/register")
-	public String addUsers(@ModelAttribute Users user) {
+	public String addUsers(@RequestBody Users user) {
 		boolean userStatus = service.emailExists(user.getEmail());
 		if(userStatus  == false) {
 			service.addUser(user);
@@ -33,28 +82,6 @@ public class UsersController {
 		
 		
 		return "home";
-	}
-	
-	@PostMapping("/validate")
-	public String validate(@RequestParam("email")String email,
-			@RequestParam("password") String password,
-			HttpSession session) {
-		
-		if(service.validateUser(email,password) == true) {
-			String role = service.getRole(email);
-			
-			session.setAttribute("email", email);
-			
-			if(role.equals("admin")) {
-				return "adminHome";
-			}
-			else {
-				return "customerHome";
-			}
-		}
-		else {
-			return "login";
-		}
 	}
 	
 	
